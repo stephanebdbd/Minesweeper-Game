8f6998fb4a18e03eebf164f95205c57ba8b02fe2
Stéphane Badi Budu <stephane.badi.budu@ulb.be>
2022-11-22 17:16:48 +0100

    Démineur

--- demineur.py
+++ demineur.py
@@ -5,6 +5,10 @@ def create_board(n, m):
     return [['' for _ in range(n)] for _ in range(m)]
 
 
+def print_board(board):
+
+
+
 def get_size(board):
     return len(board[0]), len(board)
 
@@ -41,3 +45,26 @@ def place_mines(reference_board, number_of_mines, first_pos_x, first_pos_y):
             res.append([abs_x, ord_y])
             mines += 1
     return res
+
+
+def fill_in_board(reference_board):
+
+
+
+def propagate_click(game_board, reference_board, pos_x, pos_y):
+
+
+
+def parse_input(n, m):
+
+
+
+def check_win(game_board, reference_board, mines_list, total_flags):
+
+
+
+def init_game(n, m, number_of_mines):
+
+
+
+def main():
