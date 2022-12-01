46b8ccc5b47ef718ef001180a89b365eda4781cd
TBKNbr1 <stephane.badi.budu@ulb.be>
2022-12-01 09:00:02 +0100

    Il ne me manque plus que quelques docstrings.

--- demineur.py
+++ demineur.py
@@ -44,6 +44,8 @@ def print_board(board):
                 j = u"\u001b[31mX\u001b[0m"
             elif j == 'F':
                 j = u"\u001b[32mF\u001b[0m"
+            elif j == 'Fx':
+                j = u"\u001b[31mF\u001b[0m"
             ligne += str(j) + ' '
         print(' ' * (implem - len(str(i))) + str(i) + ' | ' + str(ligne) + '|')
     print((implem + 1) * ' ' + ((2 * m) + 3) * '_')
@@ -97,16 +99,17 @@ def fill_in_board(reference_board):
     for i in range(len(reference_board)):
         for j in range(len(reference_board[0])):
             if reference_board[i][j] == '.':
-                reference_board[i][j] = str(sum(1 for k, l in get_neighbors(reference_board, j, i) if reference_board[l][k] == 'X'))
+                reference_board[i][j] = str(
+                    sum(1 for k, l in get_neighbors(reference_board, j, i) if reference_board[l][k] == 'X'))
 
 
 def propagate_click(game_board, reference_board, pos_x, pos_y):
-    (n, m), zero = get_size(game_board), []
-    if m > pos_x >= 0 <= pos_y < n:
-        game_board[pos_y][pos_x] = reference_board[pos_y][pos_x]
+    if game_board[pos_y][pos_x] != 'F':
+        game_board[pos_y][pos_x], zero = reference_board[pos_y][pos_x], []
         if reference_board[pos_y][pos_x] == '0':
             for i, j in get_neighbors(game_board, pos_x, pos_y):
-                game_board[j][i] = reference_board[j][i]
+                if game_board[j][i] != 'F':
+                    game_board[j][i] = reference_board[j][i]
                 if game_board[j][i] == '0':
                     for k, l in get_neighbors(game_board, i, j):
                         if (i, j) != (k, l) != (pos_x, pos_y) and (k, l) not in zero and game_board[l][k] != reference_board[l][k]:
@@ -117,7 +120,7 @@ def propagate_click(game_board, reference_board, pos_x, pos_y):
 
 def parse_input(n, m):
     jeu = str(input("Choix d'une case : ")).strip().split()
-    if len(jeu) == 3 and jeu[0].isalpha() and jeu[1].isdigit() and jeu[2].isdigit() and m > int(jeu[1]) >= 0 <= int(jeu[2]) < n:
+    if len(jeu) == 3 and jeu[0] == 'c' and jeu[1].isdigit() and jeu[2].isdigit() and m > int(jeu[1]) >= 0 <= int(jeu[2]) < n:
         return [jeu[0].lower(), int(jeu[2]), int(jeu[1])]
     return parse_input(n, m)
 
@@ -127,9 +130,12 @@ def check_win(game_board, reference_board, mines_list, total_flags):
     for i in range(n):
         for j in range(m):
             if game_board[i][j] == 'X':
-                for k, l in mines_list:
-                    if game_board[k][l] != 'F':
-                        game_board[k][l] = reference_board[k][l]
+                for o in range(n):
+                    for p in range(m):
+                        if game_board[o][p] != 'F' and reference_board[o][p] == 'X':
+                            game_board[o][p] = reference_board[o][p]
+                        if game_board[o][p] == 'F' and reference_board[o][p] != 'X':
+                            game_board[o][p] = 'Fx'
                 return True
             if game_board[i][j] in ['.', 'F']:
                 hide += 1
@@ -143,8 +149,11 @@ def check_win(game_board, reference_board, mines_list, total_flags):
 def init_game(n, m, number_of_mines):
     game_board, reference_board = create_board(n, m), create_board(n, m)
     print_board(game_board)
-    game = parse_input(n, m)
-    action, first_pos_x, first_pos_y = game
+    action, first_pos_x, first_pos_y = parse_input(n, m)
+    while action == 'f':
+        game_board[first_pos_y][first_pos_x] = 'F'
+        print_board(game_board)
+        action, first_pos_x, first_pos_y = parse_input(n, m)
     mines_list = place_mines(reference_board, number_of_mines, first_pos_x, first_pos_y)
     reference_board[first_pos_y][first_pos_x], game_board[first_pos_y][first_pos_x] = '0', '0'
     fill_in_board(reference_board)
@@ -158,14 +167,15 @@ def main():
     """
     Fonction principale du jeu qui contrôle le jeu
     """
-    if isinstance(sys.argv, list) and len(sys.argv) == 4:
-        if sys.argv[3].isdigit() and sys.argv[1].isdigit() and sys.argv[2].isdigit():
+    if len(sys.argv) == 4 and sys.argv[3].isdigit() and sys.argv[1].isdigit() and sys.argv[2].isdigit():
+        if int(sys.argv[1]) * int(sys.argv[2]) > int(sys.argv[3]):
             n, m, number_of_mines = int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])
             sys.setrecursionlimit(n * m)
             game_board, reference_board, mines_list = init_game(n, m, number_of_mines)
             while not check_win(game_board, reference_board, mines_list, sum(1 for i, j in mines_list if game_board[i][j] == 'F')):
                 action, pos_x, pos_y = parse_input(n, m)
                 if action == 'c':
+                    game_board[pos_y][pos_x] = reference_board[pos_y][pos_x]
                     propagate_click(game_board, reference_board, pos_x, pos_y)
                 elif action == 'f':
                     game_board[pos_y][pos_x] = 'F'
